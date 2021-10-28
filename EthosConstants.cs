25f9d1c874a6ee1700463821b7c9cff9e20602e9
Chris Craven <Chris.Craven@doane.edu>
2021-10-28 09:51:28 -0500

    Add project files.

---
+++ EthosConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EthosClient
+{
+    public class EthosConstants
+    {
+        public const string EthosBaseUrl = "https://integrate.elluciancloud.com/";
+    }
+}
---
+++ EthosIdObject.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenLearningAcademyService.Ethos.Entities
+{
+    public class GUIDObject
+    {
+
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+    }
+}
